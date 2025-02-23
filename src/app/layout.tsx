c456db9ee4263f5ab334f02a3a28fb1b32fb1013
eduardo moro <dev.eduardomoro@gmail.com>
2025-02-22 23:59:19 -0300

    Hello shadcn!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex">
-        <nav className="bg-gray-500/30 w-72 min-h-[100vh] h-full pl-12 pr-4 py-12">
-          <a>Home</a>
-        </nav>
-
-        <main className="w-full pl-4 pr-12 py-12">
-            <div className="h-full w-full">
-              <p>olá!</p>
-            </div>
-            {children}
-        </main>
+      <body className="flex bg-gray-300 h-full w-full">
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
-import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export default function Home() {
   return (
     <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <h1 className="text-2xl">hello!</h1>
+          </TooltipTrigger>
+
+          <TooltipContent>
+            <p>World!</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </>  
   );
 }
